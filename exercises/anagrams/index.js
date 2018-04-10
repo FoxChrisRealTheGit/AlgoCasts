01f664b0512f14f82b7aed33865274cf4a133054
Christopher Fox <cfox1290@gmail.com>
2018-04-10 14:50:57 -0600

    finished anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,71 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// My solution
+// function anagrams(stringA, stringB) {
+//     let map1 = {} 
+//     let map2 = {};
+//     let strA = stringA.replace(/[^\w]/, "").toLowerCase();
+//     let strB = stringB.replace(/[^\w]/, "").toLowerCase();
+//     ans = false
+//     for(let char of strA){
+//         if (map1[char]){
+//             map1[char]+=1;
+//         }else{
+//             map1[char] = 1
+//         }
+//     }
+//     for(let char of strB){
+//         if (map2[char]){
+//             map2[char]+=1;
+//         }else{
+//             map2[char] = 1
+//         }
+//     }
+//     for(let char in map2){
+//         if(map1[char]){
+//             ans = true;
+//         }else if (!map1[char]){
+//             ans = false
+//         }
+//     }
+//     return ans
+// }
+
+
+// first video solution 
+// function anagrams(stringA, stringB) {
+//     let aCharMap = buildCharMap(stringA)
+//     let bCharMap = buildCharMap(stringB)
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false
+//     }
+
+//     for(let char in aCharMap){
+//         if(aCharMap[char] !== bCharMap[char]){
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// function buildCharMap(str) {
+//     const charMap = {}
+
+//     for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap
+// }
+
+// second video solution
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str){
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+}
 
 module.exports = anagrams;
