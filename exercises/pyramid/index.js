16089a851e52767daab4462cd1237ac84bd80b3e
Christopher Fox <cfox1290@gmail.com>
2018-04-11 11:14:35 -0600

    finished pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,47 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// solution one
+// function pyramid(n) {
+//     if (n === 1) {
+//         console.log("#")
+//     }
+//     const midpoint = Math.floor((2 * n - 1) / 2)
+//     for (let row = 0; row < n; row += 1) {
+//         let level = "";
+//         for (let column = 0; column < 2 * n - 1; column += 1) {
+//             if (midpoint - row <= column && midpoint + row >= column) {
+//                 level += "#"
+//             } else {
+//                 level += " "
+//             }
+
+//         }
+//         console.log(level)
+//     }
+
+// }
+
+
+// recursive solution two 
+function pyramid(n, row = 0, level = "") {
+    if (row === n) {
+        return
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1)
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2)
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = "#"
+    } else {
+        add = " "
+    }
+    pyramid(n, row, level +add)
+}
 
 module.exports = pyramid;
