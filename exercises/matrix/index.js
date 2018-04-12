3713d65868f717b147f2874a2e1ef1855cc2b276
Christopher Fox <cfox1290@gmail.com>
2018-04-12 12:42:46 -0600

    finished spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+    for(let i = 0; i <n; i+=1){
+        results.push([]);
+    }
+
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n-1;
+    let startRow = 0;
+    let endRow = n-1;
+
+    while (startColumn <= endColumn && startRow <= endRow){
+        //top row
+        for(let i = startColumn; i<= endColumn; i+=1 ){
+            results[startRow][i] = counter;
+            counter +=1
+        }
+        startRow +=1;
+
+        // right column
+        for(let i = startRow; i <= endRow; i+=1){
+            results[i][endColumn] = counter;
+            counter +=1
+        }
+        endColumn -=1;
+
+        //bottom row
+        for(let i = endColumn; i>= startColumn; i-=1){
+            results[endRow][i] = counter;
+            counter +=1
+        }
+        endRow -=1;
+
+        // start column
+        for(let i = endRow; i>= startRow; i-=1){
+            results[i][startColumn] = counter;
+            counter +=1
+        }
+        startColumn +=1
+    }
+    return results
+}
 
 module.exports = matrix;
